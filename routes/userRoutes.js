f8c134636b6324e51da310fe02ff2a5b581c77b9
Alberto Juan Gabino <intrunet@gmail.com>
2024-09-30 21:34:54 -0600

    Se corrigen metodo de createUser

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 
 router.get('/', userController.getUsers);
-router.post('/create', userController.createUsers);
+router.post('/create', userController.createUser);
 router.post('/delete/:id', userController.deleteUser);
 router.post('/assign-license/:userId', userController.assignLicense);
 
