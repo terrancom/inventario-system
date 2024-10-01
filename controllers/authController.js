5f315a8da20c04a3b5b3d1afb4439ba74b8725b8
Alberto Juan Gabino <intrunet@gmail.com>
2024-09-30 18:25:37 -0600

    Se agrega inicio de sesion y proteccion de rutas

---
+++ controllers/authController.js
@@ -0,0 +1,43 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.login = async (req, res) => {
+    const {
+        username,
+        password
+    } = req.body;
+
+    const user = await User.findOne({
+        username
+    });
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+        return res.status(400).json({
+            message: 'Datos incorrectos'
+        });
+    }
+
+    const token = jwt.sign({
+        userId: user._id, 
+        role: user.role
+    }, process.env.JWT_SECRET); 
+    req.session.token = token;
+
+    res.redirect('/dashboard');
+};
+
+exports.protect = (req, res, next) =>{
+    const token = req.session.token;
+    if (!token) {
+        return res.redirect('/auth/login');
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    }catch(err){
+        return res.redirect('/auth/login');
+    }
+};
